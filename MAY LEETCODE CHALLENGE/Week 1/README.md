ba9c5b73140c9de695aea023737ab44e2dccc3d4
Suryank Dixit <49695800+SuryankDixit@users.noreply.github.com>
2020-05-02 19:58:29 +0530

    Initial Commit.

---
+++ MAY LEETCODE CHALLENGE/Week 1/README.md
@@ -0,0 +1,4 @@
+# WEEK 1:
+
+1. Bad version : It is a simple binary search problem.
+2. Stones and Jewels: We can solve this by counting the occurrence of characters. (Can use hash tables.)
---
+++ MAY LEETCODE CHALLENGE/Week 1/bad_version.cpp
@@ -0,0 +1,48 @@
+
+/*
+Question:
+
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+Example:
+
+Given n = 5, and version = 4 is the first bad version.
+
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+
+Then 4 is the first bad version. 
+
+
+*/
+
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        if(n==1)
+            return 1;
+        int start=1;
+        int end=n;
+        int mid;
+        while(start<end)
+        {
+            mid=start+(end-start)/2;
+            if(isBadVersion(mid))
+            {
+                end=mid;
+            }
+            else
+                start=mid+1;
+        }
+        return start;
+    }
+};
