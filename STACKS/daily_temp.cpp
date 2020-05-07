18ea01381f19e4a6f94f1641e5e25b214e546288
Suryank Dixit <49695800+SuryankDixit@users.noreply.github.com>
2020-05-07 21:12:19 +0530

    Create daily_temp.cpp

---
+++ STACKS/daily_temp.cpp
@@ -0,0 +1,27 @@
+
+//739. Daily Temperatures
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& T) {
+        
+        // variation of next greater element in right.
+        stack<int> s;
+        //vector<int> res(T.size());
+        s.push(0);
+        for(int i=1;i<T.size();i++)
+        {
+            while(!s.empty() && T[s.top()]<T[i])
+            {
+                T[s.top()]=i-s.top();
+                s.pop();
+            }
+            s.push(i);
+        }
+        while(!s.empty())
+        {
+            T[s.top()]=0;
+            s.pop();
+        }
+        return T;
+    }
+};
