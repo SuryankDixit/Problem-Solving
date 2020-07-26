840dbd09eab4caa328618d6d7176656c71617983
Suryank Dixit <49695800+SuryankDixit@users.noreply.github.com>
2020-07-26 22:15:10 +0530

    Create lca.cpp

---
+++ TREE/lca.cpp
@@ -0,0 +1,32 @@
+Time => O(n)
+Space => O(1)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+       if(root == NULL)
+            return NULL;
+        if(root->val == p->val || root->val == q->val)
+            return root;
+        
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+        
+        if(left == NULL)
+            return right;
+        if(right == NULL)
+            return left;
+        
+        return root;
+        }
+        }};
