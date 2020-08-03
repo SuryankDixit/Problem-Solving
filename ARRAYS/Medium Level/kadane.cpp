641fee92e2e69fd21f4621a144d3b0af71d45b29
Suryank Dixit <49695800+SuryankDixit@users.noreply.github.com>
2020-08-03 22:16:40 +0530

    Create kadane.cpp

---
+++ ARRAYS/Medium Level/kadane.cpp
@@ -0,0 +1,23 @@
+// Function to find subarray with maximum sum
+// arr: input array
+// n: size of array
+int maxSubarraySum(int arr[], int n){
+    
+    
+    int local=0;
+    int global = 0;
+    int sum = INT_MIN;
+    for(int i=0;i<n;i++){
+        
+        local = arr[i];
+        global = global + arr[i];
+        if(local > global)
+            global = local;
+            
+        sum = max(sum,global);
+   }
+   return sum;
+    
+    // Your code here
+    
+}
