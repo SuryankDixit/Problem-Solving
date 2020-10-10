98f981881d43959d3b726e71a0e37210b849d67f
Suryank Dixit <49695800+SuryankDixit@users.noreply.github.com>
2020-10-10 20:06:06 +0530

    Update 0-1memoized_recursive_knapsack.cpp

--- DYNAMIC P/0-1 Knapsack Patterns/0-1memoized_recursive_knapsack.cpp
+++ DYNAMIC P/0-1 Knapsack Patterns/0-1memoized_recursive_knapsack.cpp
@@ -1,35 +1,48 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int knapsack(vector<int> &val, vector<int> &wt, int n,int w)
-{
-    if(n==0 || w==0)
+
+int dfs_knapsack(int n, int w,vector<int> &val, vector<int> &wt,vector<vector<int>> &dp){
+
+    // cout<<"("<<n-1<<","<<w<<")"<<endl;
+    if(n==0|| w==0)
         return 0;
+
+    if(dp[n-1][w-1]!=-1){
+        return dp[n-1][w-1];
+    }
     
-    if(wt[n-1]<=w)
-        return max(val[n-1]+knapsack(val,wt,n-1,w-wt[n-1]),knapsack(val,wt,n-1,w));
-        
-    else if(wt[n-1]>w)
-        return knapsack(val,wt,n-1,w);
+    if(w>=wt[n-1]){
+        return dp[n-1][w-1] = max(dfs_knapsack(n-1,w-wt[n-1],val,wt,dp)+val[n-1],dfs_knapsack(n-1,w
+            ,val,wt,dp));
+    }else{
+        return dp[n-1][w-1] = dfs_knapsack(n-1,w,val,wt,dp);
+    }
 }
 
+
 int main() {
-	//code
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    int n,w;
-	    cin>>n>>w;
-	    vector<int> val(n);
-	    vector<int> wt(n);
-	    for(int i=0;i<n;i++)
-	        cin>>val[i];
-	    for(int i=0;i<n;i++)
-	        cin>>wt[i];
-	    
-	    int max_profit=knapsack(val,wt,n,w);
-	    cout<<max_profit<<endl;
-	}
-	return 0;
+    
+
+     #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+    vector<int> wt(n);
+    vector<int> val(n);
+    for(int i=0;i<n;i++)
+        cin>>wt[i];
+    for(int i=0;i<n;i++)
+        cin>>val[i];
+    int W;
+    cin>>W;
+
+    vector<vector<int>> dp(n,vector<int>(W,-1));
+
+    cout<<dfs_knapsack(n,W,val,wt,dp);
 }
+
