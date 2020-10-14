4ba87e2504aae3368ea49b7b7a3e581d8bdd9e6c
Suryank Dixit <49695800+SuryankDixit@users.noreply.github.com>
2020-10-14 23:41:03 +0530

    decode_ways_bottom_up

---
+++ DYNAMIC P/Fibonacci Pattern/lc91.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    int numDecodings(string s) {
+        
+        int n = s.size();
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        if(s[0] != '0')
+            dp[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            if(s[i-1] != '0')
+                dp[i]+=dp[i-1];
+            if(s[i-2] =='1' || (s[i-2] =='2' && s[i-1]<='6'))
+                dp[i]+=dp[i-2];
+        }
+        return dp[n];
+    }
+};
