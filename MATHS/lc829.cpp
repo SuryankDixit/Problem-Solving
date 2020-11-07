137d42feabf1b9260b68abcf9325bf9d478bc15a
Suryank Dixit <49695800+SuryankDixit@users.noreply.github.com>
2020-11-07 10:06:29 +0530

    Create lc829.cpp

---
+++ MATHS/lc829.cpp
@@ -0,0 +1,26 @@
+/*
+Given a positive integer N, how many ways can we write it as a sum of consecutive positive integers?
+
+Example 1:
+
+Input: 5
+Output: 2
+Explanation: 5 = 5 = 2 + 3
+Example 2:
+
+Input: 9
+Output: 3
+Explanation: 9 = 9 = 4 + 5 = 2 + 3 + 4
+*/
+
+class Solution {
+public:
+    int consecutiveNumbersSum(int N) {
+        
+        int count = 1;
+        for( int k = 2; k < sqrt( 2 * N ); k++ ) {
+            if ( ( N - ( k * ( k - 1 )/2) ) % k == 0) count++;
+        }
+        return count;
+    }
+};
