c8c5e10b17b8a5e7582cd7ac9c6f89fc24457436
Suryank Dixit <49695800+SuryankDixit@users.noreply.github.com>
2020-12-30 19:05:04 +0530

    Create 7-Cycle-rotate.cpp

---
+++ ARRAYS/DSA/7-Cycle-rotate.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    int x=a[n-1];
+    for(int i=n-1;i>0;i--){
+        a[i]=a[i-1];
+    }
+    a[0]=x;
+    for(auto c:a)
+        cout<<c<<" ";
+    cout<<endl;
+}
+
+
+
+int main() {
+ 
+    // #ifndef ONLINE_JUDGE    
+    //     freopen("input.txt","r",stdin);
+    //     freopen("output.txt","w",stdout);
+    // #endif
+
+    int t;
+    cin>>t;
+    while(t--){
+    	solve();
+    }
+}
+
