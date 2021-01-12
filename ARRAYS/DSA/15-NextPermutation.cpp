3eba6e864eb6b572db3fb645b2db81247ce30b8c
Suryank Dixit <49695800+SuryankDixit@users.noreply.github.com>
2021-01-12 22:49:15 +0530

    leetcode-31

--- ARRAYS/DSA/15-NextPermutation.cpp
+++ ARRAYS/DSA/15-NextPermutation.cpp
@@ -1,40 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-
         int n = nums.size();
-        int idx=-1,idx2,temp;
+        int idx1=-1;
+        int idx2;
         
-        if(n==1 || n==0)
-            return;
-
-        for(int i = n-1;i>0;i--){
-            if(nums[i-1]<nums[i]){
-                idx=i-1;
-                temp= nums[i-1];
+        for(int i=n-1;i>0;i--){
+            if(nums[i]>nums[i-1]){
+                idx1=i-1;
                 break;
             }
         }
-         if(idx==-1){
-             reverse(nums.begin(),nums.end());
-             return;
-         }
-        cout<<idx<<" "<<temp<<endl;
-        int x=nums[n-1];
-        for(int i=n-1;i>idx;i--){
-            if((x>nums[i] && x>temp) || x<=temp){
-                x=nums[i];
+        
+        if(idx1==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        
+        int temp = nums[idx1];
+        for(int i=n-1;i>idx1;i--){
+            if(nums[i]>temp){
                 idx2=i;
+                break;
             }
         }
-        cout<<x<<" "<<idx2<<endl;
-        swap(nums[idx],nums[idx2]);
-        int i = idx+1;
-        int j = n-1;
-        while(i<=j){
-            swap(nums[i],nums[j]);
-            i++;
-            j--;
-        }
+        
+        swap(nums[idx1],nums[idx2]);
+        int i=idx1+1;
+        reverse(nums.begin()+i,nums.end());
+        
+        return;
     }
 };
