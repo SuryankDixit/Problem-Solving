c6baa6e49b4bc29437b7bbea06b7e8c36af98ea2
Suryank Dixit <49695800+SuryankDixit@users.noreply.github.com>
2021-01-13 19:31:55 +0530

    Create 31-Smallest-sub-array-with-sum-greater-than-x.cpp

---
+++ ARRAYS/DSA/31-Smallest-sub-array-with-sum-greater-than-x.cpp
@@ -0,0 +1,17 @@
+
+
+int sb(int arr[], int n, int x)
+{
+    // Your code goes here   
+    int start = 0;
+    int sum=0;
+    int Min=INT_MAX;
+    for(int end=0;end<n;end++){
+        sum+=arr[end];
+        while(sum>x){
+            sum-=arr[start++];
+            Min=min(Min,end-start+2);
+        }
+    }
+    return Min;
+}
