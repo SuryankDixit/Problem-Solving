4dc47f01d34df41bb439c43a914948f88960a108
suryankdixit <suryankdixit@gmail.com>
2021-01-27 15:24:18 +0530

    lc 102

---
+++ TREE/level-order-bfs/level-order-binary-tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root ==NULL)
+            return ans;
+        queue<TreeNode *> q;
+        q.push(root);
+        while(!q.empty()){
+            int k = q.size();
+            vector<int> v;
+            while(k--){
+                TreeNode* temp = q.front();
+                q.pop();
+                v.push_back(temp->val);
+                if(temp->left!=NULL)
+                    q.push(temp->left);
+                if(temp->right!=NULL)
+                    q.push(temp->right);
+            }
+            ans.push_back(v);
+        }
+        return ans;
+    }
+};
