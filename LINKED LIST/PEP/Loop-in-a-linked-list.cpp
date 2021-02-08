9107533ffbd269d94cd145e19402febbbb70bc57
suryankdixit <suryankdixit@gmail.com>
2021-02-08 22:13:34 +0530

    https://practice.geeksforgeeks.org/problems/remove-loop-in-linked-list/1#

---
+++ LINKED LIST/PEP/Loop-in-a-linked-list.cpp
@@ -0,0 +1,154 @@
+// driver code
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+bool isLoop(Node* head)
+{
+    if(!head) return false;
+    
+    Node* fast = head->next;
+    Node* slow = head;
+    
+    while( fast != slow)
+    {
+        if( !fast || !fast->next ) return false;
+        fast=fast->next->next;
+        slow=slow->next;
+    }
+    
+    return true;
+}
+
+int length(Node* head)
+{
+    int ret = 0;
+    while(head)
+    {
+        ret++;
+        head = head->next;
+    }
+    return ret;
+}
+
+void removeLoop(Node* head);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        removeLoop(head);
+        
+        if( isLoop(head) || length(head)!=n )
+            cout<<"0\n";
+        else
+            cout<<"1\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*
+structure of linked list node:
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+*/
+
+void removeLoop(Node* head)
+{
+    
+    Node* slow=head;
+    Node* fast=head;
+     Node* prev=nullptr;
+    if(head==nullptr||head->next==nullptr)
+    return ;
+    while(fast!=nullptr)
+    {
+        slow=slow->next;
+        if(fast->next!=nullptr)
+        {   prev=fast->next;
+            fast=fast->next->next;
+        }
+        else
+        break;
+        
+        if(slow==fast)
+        break;
+    }
+    if(slow==fast)
+    {
+        if(fast==head)
+        {
+            prev->next=nullptr;
+            return ;
+        }
+        slow=head;
+        prev=fast;
+        while(slow!=fast)
+        {
+            slow=slow->next;
+            prev=fast;
+            fast=fast->next;
+            
+            
+        }
+        prev->next=nullptr;
+        
+    }
+    return ;
+    
+}
