38cf4b0d7623e30ce10f24ca119cb4fcd13ade4b
suryankdixit <suryankdixit@gmail.com>
2021-02-28 23:02:08 +0530

    https://leetcode.com/problems/distinct-subsequences/

---
+++ DYNAMIC P/Dp on Strings/lc-115-distinct-subsequences.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    typedef unsigned long long int ll;
+    int numDistinct(string s, string t) {
+        int n1=s.size()+1;
+        int n2=t.size()+1;
+        if(n1<n2)
+            return 0;
+        
+        vector<vector<ll>> dp(n2,vector<ll>(n1,0));
+        
+        for(int i=1;i<n1;i++){
+            if(t[0]==s[i-1])
+                dp[1][i]=dp[1][i-1]+1;
+            else{
+                dp[1][i]=dp[1][i-1];
+            }
+        }
+        for(int i=2;i<n2;i++){
+            for(int j=1;j<n1;j++){
+                if(s[j-1] != t[i-1])
+                    dp[i][j]=dp[i][j-1];
+                else{
+                    dp[i][j]=dp[i-1][j-1]+dp[i][j-1];
+                } 
+            }
+        }
+        return dp[n2-1][n1-1];
+    }
+};
