dd5cee6fd681ff0ee9f71c5114faf8e73da45f21
suryankdixit <suryankdixit@gmail.com>
2021-03-10 22:11:31 +0530

    https://leetcode.com/problems/design-hashmap/

---
+++ HashTable/lc706-design-hash.cpp
@@ -0,0 +1,31 @@
+class MyHashMap {
+public:
+    /** Initialize your data structure here. */
+    vector<int> v;
+    MyHashMap() {
+        v.resize(1000000+7,-1);
+    }
+    
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        v[key] = value;
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        return v[key];
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        v[key]=-1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
