6eb366563401428e4685d17d84f58034290a16b7
suryankdixit <suryankdixit@gmail.com>
2021-04-05 01:59:14 +0530

    https://leetcode.com/problems/minimum-increment-to-make-array-unique/

--- ARRAYS/Medium Level/lc-945-min-increment-to-make-array-unique.cpp
+++ ARRAYS/Medium Level/lc-945-min-increment-to-make-array-unique.cpp
@@ -1,3 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& A) {
@@ -13,3 +16,21 @@ public:
         return ans;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int minIncrementForUnique(vector<int>& A) {
+        
+//         sort(A.begin(), A.end());
+//         int ans = 0;                
+//         for(int i=1;i<A.size();i++){
+//             if(A[i]<=A[i-1]){
+//                 ans+=A[i-1]+1-A[i];
+//                 A[i] = A[i-1]+1;
+//             }
+//         }
+//         return ans;
+//     }
+// };
