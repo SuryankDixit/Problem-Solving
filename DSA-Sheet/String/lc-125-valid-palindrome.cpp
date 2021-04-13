783f4238cf3341d513edaf35536dbcc23f1f1f86
suryankdixit <suryankdixit@gmail.com>
2021-04-13 22:11:58 +0530

    https://leetcode.com/problems/valid-palindrome/

---
+++ DSA-Sheet/String/lc-125-valid-palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i=0;int j=s.size()-1;
+        while(i<j){
+            while(i<j && isalnum(s[i])==false)
+                i++;
+            while(i<j && isalnum(s[j])==false)
+                j--;
+            if(tolower(s[i])!=tolower(s[j]))
+                return false;
+            i++;j--;
+        }
+        return true;
+    }
+};
