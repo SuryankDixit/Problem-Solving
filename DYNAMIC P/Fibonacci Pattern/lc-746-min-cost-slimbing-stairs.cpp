32620b5f00e982e680e2b0fd3efa1cd27151cc00
suryankdixit <suryankdixit@gmail.com>
2021-05-14 10:28:06 +0530

    https://leetcode.com/problems/min-cost-climbing-stairs/submissions/

--- DYNAMIC P/Fibonacci Pattern/lc-746-min-cost-slimbing-stairs.cpp
+++ DYNAMIC P/Fibonacci Pattern/lc-746-min-cost-slimbing-stairs.cpp
@@ -21,3 +21,24 @@ public:
         return min(dfs(cost,n-1),dfs(cost,n-2));
    }
 };
+
+
+//bottom up
+class Solution {
+public:
+
+    int minCostClimbingStairs(vector<int>& c) {
+        int n = c.size();
+        if(n==2)
+            return min(c[0],c[1]);
+        
+        vector<int> dp(n,-1);
+        for(int i=0;i<n;i++){
+            if(i<2)
+                dp[i]=c[i];
+            else
+                dp[i]=c[i]+min(dp[i-1],dp[i-2]);
+        }
+        return min(dp[n-1],dp[n-2]);
+   }
+};
