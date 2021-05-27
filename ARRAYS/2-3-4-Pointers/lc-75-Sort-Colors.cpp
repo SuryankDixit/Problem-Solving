520f65ddaa2a03b77ddda9ef4a99913fbf647b10
SuryankDixit <suryankdixit@gmail.com>
2021-05-27 23:51:50 +0530

    Update

---
+++ ARRAYS/2-3-4-Pointers/lc-75-Sort-Colors.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int i=0;int j=n-1;int k=0;
+        while(k<=j){
+            if(nums[k]==0){
+                swap(nums[k],nums[i]);i++;k++;}
+            else if(nums[k]==2){
+                swap(nums[k],nums[j]);j--;}
+            else
+                k++;
+        }
+    }
+};
