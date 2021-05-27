86cce850ae046cb7a2cd15e60ad64771efda27a1
SuryankDixit <suryankdixit@gmail.com>
2021-05-28 00:19:13 +0530

    https://leetcode.com/problems/is-graph-bipartite/submissions/

--- GRAPH/Bipartite Graph/checkBipartite.cpp
+++ GRAPH/Bipartite Graph/checkBipartite.cpp
@@ -3,45 +3,34 @@ using namespace std;
 typedef long long int ll;
 const int N = 100005;
 
-int flag=0;
-
-vector<int> graph[N];
-
-void checkBipartite(int node,vector<int> &visited,int parent,int color){
-
-    visited[node] = color;
-
-    for(auto child:graph[node]){
-
-        if(visited[child]==false){
-            checkBipartite(child,visited,node,3-color);
+class Solution {
+public:
+    vector<int> visited;
+    
+    bool dfs(int s,int color,vector<vector<int>>& graph){
+        visited[s]=color;
+        for(auto x:graph[s]){
+            if(visited[x]==0){
+                bool f = dfs(x,3-color,graph);
+                if(f == false)
+                    return false;
+            }else if(visited[x]==visited[s]){
+                return false;
+            }
         }
-        else if(child!=parent && visited[child]==color)
-            flag=1;
+        return true;
     }
-}
-
-int main() {
-
-    int vertex,edges;
-    cin>>vertex>>edges;
-
-    for(int i=0;i<edges;i++){
-        int x,y;
-        cin>>x>>y;
-        graph[x].push_back(y);
-        graph[y].push_back(x);
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        visited.resize(101,0);
+        for(int i=0;i<n;i++){
+            if(visited[i]==0){
+                bool status = dfs(i,1,graph);
+                if(!status)
+                    return false;
+            }
+        }
+        return true;
     }
-
-    vector<int> visited(N);
-    // apply dfs on every node and check whether it is bipartite or not;
-    for(int i=1;i<=vertex;i++){
-        if(visited[i]==0)
-            checkBipartite(i,visited,-1,1);
-    }
-    if(flag==0)
-        cout<<"Bipartite Graph";
-    else
-        cout<<"Not a bipartite graph";
-    return 0;
-}
+};
