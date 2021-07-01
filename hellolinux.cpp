e1f456516ff478ba7c5a8b92466ecbc581cc81db
SuryankDixit <suryankdixit@gmail.com>
2021-07-01 08:20:49 +0530

    Updated

--- hellolinux.cpp
+++ hellolinux.cpp
@@ -1,7 +1,9 @@
-#include<bits/stdc++.h>
-uisng namespace std;
+#include <iostream>
+using namespace std;
 
-int main(){
-	cout<<"HELLO LINUX.";
+int main()
+{
+	cout<<"Welcome to Linux Terminal Git.(modified)";
+	cout<<"Modifying from VS code.";
 	return 0;
 }
