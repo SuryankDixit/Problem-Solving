a5caba1158d6b403a3a9ac94437e450c72c3e7ad
SuryankDixit <suryankdixit@gmail.com>
2021-07-10 22:16:09 +0530

    https://leetcode.com/problems/maximum-product-subarray/

--- DSA-Sheet/Arrays/23-max-product-subarray.cpp
+++ DSA-Sheet/Arrays/23-max-product-subarray.cpp
@@ -29,3 +29,33 @@ public:
         return Max;
 	}
 };
+
+
+/*
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        vector<int> v(nums.size(),1);
+        v[0]=nums[0];
+        int ans = v[0];
+        if(v[0]==0)
+            v[0]=1;
+        for(int i=1;i<nums.size();i++){
+            v[i]=v[i-1]*nums[i];
+            ans = max(ans,v[i]);
+            if(v[i]==0)
+                v[i]=1;
+        }
+        ans = max(ans,nums[nums.size()-1]);
+        if(nums[nums.size()-1]==0)
+            nums[nums.size()-1]=1;
+        for(int i=nums.size()-2;i>=0;i--){
+            nums[i]=nums[i+1]*nums[i];
+            ans=max(ans,nums[i]);
+            if(nums[i]==0)
+                nums[i]=1;
+        }
+        return ans;
+    }
+};
+*/
