ff79ec9ecce1168e5d777f23537f1c8d5b2b1bad
SuryankDixit <suryankdixit@gmail.com>
2021-09-09 22:01:29 +0530

    https://www.interviewbit.com/problems/maximum-absolute-difference/

---
+++ ARRAYS/max-absolute-diff-IB.cpp
@@ -0,0 +1,15 @@
+int Solution::maxArr(vector<int> &arr)
+{
+    int n = arr.size();
+    int max1 = INT_MIN, min1 = INT_MAX;
+    int max2 = INT_MIN, min2 = INT_MAX;
+
+    for (int i = 0; i < n; i++)
+    {
+        max1 = max(max1, arr[i] + i);
+        min1 = min(min1, arr[i] + i);
+        max2 = max(max2, arr[i] - i);
+        min2 = min(min2, arr[i] - i);
+    }
+    return max(max1 - min1, max2 - min2);
+}
